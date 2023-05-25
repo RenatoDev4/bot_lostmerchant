84b93f60d7ee50fb65a1ab25eb416cbc01cfd2f9
Renato Moraes <renatodev4@gmail.com>
2023-05-25 12:26:07 -0300

    Add new servers

--- run_bot.py
+++ run_bot.py
@@ -1,4 +1,16 @@
+import time
+
 from main_kazeros_heroku import run_bot
 
-run_bot("South America", "Kazeros", '-1001538174798',
-        '/home/renato/bot_lostmerchants/sent_messages_kazeros.txt')
+parameters = [
+    ("South America", "Kazeros", '-1001538174798',
+     '/bot_lostmerchants/sent_messages_kazeros.txt'),
+    ("South America", "Arthetine", '-1001878906754',
+     '/bot_lostmerchants/sent_messages_arthetine.txt'),
+    ("South America", "Blackfang", '-1001868489245',
+     '/bot_lostmerchants/sent_messages_blackfang.txt'),
+]
+
+for params in parameters:
+    run_bot(*params)
+    time.sleep(1)
