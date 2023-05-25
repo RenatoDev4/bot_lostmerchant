6e6cb3bd53fa3ab048c96857ec1412a67b12d591
Renato Moraes <renatodev4@gmail.com>
2023-05-25 12:44:45 -0300

    Fix

--- run_bot.py
+++ run_bot.py
@@ -3,12 +3,9 @@ import time
 from main_kazeros_heroku import run_bot
 
 parameters = [
-    ("South America", "Kazeros", "Kazeros / SA", '-1001538174798',
-     'sent_messages_kazeros.txt'),
-    ("South America", "Arthetine", "Arthetine / SA", '-1001878906754',
-     'sent_messages_arthetine.txt'),
-    ("South America", "Blackfang", "Blackfang / SA", '-1001868489245',
-     'sent_messages_blackfang.txt'),
+    ("South America", "Kazeros", "Kazeros / SA", '-1001538174798'),
+    ("South America", "Arthetine", "Arthetine / SA", '-1001878906754'),
+    ("South America", "Blackfang", "Blackfang / SA", '-1001868489245'),
 ]
 
 for params in parameters:
